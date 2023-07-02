f16f55dffb412181d3340d809c09c313d5af6963
Daniel <kkdh1109@gmail.com>
2023-07-01 21:08:12 -0400

    use update and cache.readQuery/writhQuery to update ui upon deleting the client

--- client/src/App.js
+++ client/src/App.js
@@ -2,9 +2,29 @@ import Header from "./components/Header";
 import { ApolloProvider, ApolloClient, InMemoryCache } from "@apollo/client";
 import Clients from "./components/Clients";
 
+//to fix merge warning while caching updated client/project data
+const cache = new InMemoryCache({
+  typePolicies: {
+    Query: {
+      fields: {
+        clients: {
+          merge(existing, incoming) {
+            return incoming;
+          },
+        },
+        projects: {
+          merge(existing, incoming) {
+            return incoming;
+          },
+        },
+      },
+    },
+  },
+});
+
 const client = new ApolloClient({
-  uri: 'http://localhost:8000/graphql',
-  cache: new InMemoryCache(),
+  uri: "http://localhost:8000/graphql",
+  cache: cache,
 });
 
 function App() {
